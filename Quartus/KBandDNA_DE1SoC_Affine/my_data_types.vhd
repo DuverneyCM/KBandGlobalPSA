38f6528f15dc8093aef9de85180626209a802894
DuverneyCM <duverney.corrales@correounivalle.edu.co>
2018-09-21 19:55:45 -0500

    Update and sort the affine gap projects. Now, ready for start :)

--- Quartus/KBandDNA_DE1SoC_Affine/my_data_types.vhd
+++
@@ -1,6 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-package my_data_types is
-               constant m : integer := 2;       -- size in bits of the arrows
-                type matrix is array (natural range <>) of std_logic_vector (m-1 downto 0);
-end my_data_types;
